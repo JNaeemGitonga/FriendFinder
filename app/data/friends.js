1f797d62f61078d109f055c202ea6868afd7269e
gauguin <chrismayo23@gmail.com>
2018-08-22 17:17:26 -0400

    added to javascript

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,20 +6,23 @@
 // ===============================================================================
 
 var friendsArray = [
-    {
-      q1: 1,
-      q2: 4,
-      q3: 3,
-      q4: 5,
-      q5: 2,
-      q6: 5,
-      q7: 2,
-      q8: 1,
-      q9: 4,
-      q10: 3,
-    }
-  ];
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friendsArray;
-  
+  {
+    name: "Sara",
+    photo: "google.com",
+    scores: [
+      1,
+      4,
+      3,
+      5,
+      2,
+      5,
+      2,
+      1,
+      4,
+      3
+    ]
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
